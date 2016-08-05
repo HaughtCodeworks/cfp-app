dba58ee5e379f076f1b93781c53765ad6ff6e876
PenneyGadget <dreamofgreen@gmail.com>
2016-08-05 10:55:30 -0600

    Organizes events more logically in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,19 @@
 pwd = "userpass"
-seed_start_date = 8.months.from_now
-sapphire_start_date = 10.months.from_now
 
 ## Users
-admin = User.create(name: "Admin", email: "an@admin.com", admin: true, password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
-organizer = User.create(name: "Event MC", email: "mc@seed.event", password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
-track_director = User.create(name: "Track Director", email: "track@seed.event", password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
-reviewer = User.create(name: "Reviewer", email: "review@seed.event", password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
+admin            = User.create(name: "Admin", email: "an@admin.com", admin: true, password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
+organizer        = User.create(name: "Event MC", email: "mc@seed.event", password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
+track_director   = User.create(name: "Track Director", email: "track@seed.event", password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
+reviewer         = User.create(name: "Reviewer", email: "review@seed.event", password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
 speaker_reviewer = User.create(name: "Speak and Review", email: "both@seed.event", password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
-speaker_1 = User.create(name: "Speaker1", email: "speak1@seed.event", password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
-speaker_2 = User.create(name: "Speaker2", email: "speak2@seed.event", password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
-speaker_3 = User.create(name: "Speaker3", email: "speak3@seed.event", password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
-speaker_4 = User.create(name: "Speaker4", email: "speak4@seed.event", password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
-speaker_5 = User.create(name: "Speaker5", email: "speak5@seed.event", password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
+speaker_1        = User.create(name: "Speaker1", email: "speak1@seed.event", password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
+speaker_2        = User.create(name: "Speaker2", email: "speak2@seed.event", password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
+speaker_3        = User.create(name: "Speaker3", email: "speak3@seed.event", password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
+speaker_4        = User.create(name: "Speaker4", email: "speak4@seed.event", password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
+speaker_5        = User.create(name: "Speaker5", email: "speak5@seed.event", password: pwd, password_confirmation: pwd, confirmed_at: Time.now)
+
+### SeedConf -- event is in the middle of the CFP
+seed_start_date = 8.months.from_now
 
 # Core Event Info
 seed_guidelines = %Q[
@@ -35,31 +36,10 @@ seed_event = Event.create(name: "SeedConf",
                           guidelines: seed_guidelines,
                           proposal_tags: %w(beginner intermediate advanced))
 
-sapphire_guidelines = %Q[
-# SapphireConf - The place to be in 2017!
-
-The first annual conference about the hot new programming language Sapphire
-is happening in moody Reykjavík, Iceland on February 1st, 2017!
-
-If you are on the cutting edge with savvy Sapphire skills, we want you!
-]
-
-sapphire_event = Event.create(name: "SapphireConf",
-                              slug: "sapphireconf",
-                              url: "https://en.wikipedia.org/wiki/Sapphire",
-                              contact_email: "info@sapphire.event",
-                              closes_at: 8.months.from_now,
-                              state: "draft",
-                              start_date: sapphire_start_date,
-                              end_date: sapphire_start_date + 1,
-                              guidelines: sapphire_guidelines,
-                              proposal_tags: %w(beginner intermediate advanced),
-                              review_tags: %w(beginner intermediate advanced))
-
 # Session Formats
-lightning_talk = seed_event.public_session_formats.create(name: "Lightning Talk", duration: 5, description: "Warp speed! Live your audience breathless and thirsty for more.")
-short_session_format = seed_event.public_session_formats.create(name: "Short Talk", duration: 40, description: "Kinda short! Talk fast. Talk hard.")
-long_session_format = seed_event.public_session_formats.create(name: "Long Talk", duration: 120, description: "Longer talk allows a speaker put more space in between words, hand motions.")
+lightning_talk          = seed_event.public_session_formats.create(name: "Lightning Talk", duration: 5, description: "Warp speed! Live your audience breathless and thirsty for more.")
+short_session_format    = seed_event.public_session_formats.create(name: "Short Talk", duration: 40, description: "Kinda short! Talk fast. Talk hard.")
+long_session_format     = seed_event.public_session_formats.create(name: "Long Talk", duration: 120, description: "Longer talk allows a speaker put more space in between words, hand motions.")
 internal_session_format = seed_event.session_formats.create(name: "Beenote", public: false, duration: 180, description: "Involves live bees.")
 
 # Tracks
@@ -67,31 +47,18 @@ track_1 = seed_event.tracks.create(name: "Best Track", description: "Better than
 track_2 = seed_event.tracks.create(name: "OK Track", description: "The track is okay.", guidelines: "Mediocrity breeds mediocrity. Let's talk about how to transcend the status quo.")
 track_3 = seed_event.tracks.create(name: "Boring Track", description: "Great if you want a nap!", guidelines: "Sleep deprivation is linked to many health problem. Get healthy here so you can be 100% for the Best Track.")
 
-# SeedConf Rooms
-room_1 = seed_event.rooms.create(name: "Auditorium", room_number: "1000-AUD", level: "1", address: "1510 Blake St", capacity: 300, grid_position: 1, event_id: seed_event.id)
-room_2 = seed_event.rooms.create(name: "Atrium", room_number: "1000-ATR", level: "1", address: "1510 Blake St", capacity: 150, grid_position: 2, event_id: seed_event.id)
-room_3 = seed_event.rooms.create(name: "Classroom A", room_number: "2100", level: "2", address: "1400 Market St", capacity: 75, grid_position: 3, event_id: seed_event.id)
-room_4 = seed_event.rooms.create(name: "Classroom P", room_number: "3500", level: "3", address: "1510 Blake St", capacity: 50, grid_position: 4, event_id: seed_event.id)
-room_5 = seed_event.rooms.create(name: "Lobby", room_number: "LOB", level: "1", address: "1400 Market St", capacity: 250, event_id: seed_event.id)
+# Rooms
+room_1 = seed_event.rooms.create(name: "Sun Room", room_number: "SUN", level: "12", address: "123 Universe Drive", capacity: 300, event_id: seed_event.id)
+room_2 = seed_event.rooms.create(name: "Moon Room", room_number: "MOON", level: "6", address: "123 Universe Drive", capacity: 150, event_id: seed_event.id)
+room_3 = seed_event.rooms.create(name: "Venus Theater", room_number: "VEN-T", level: "2", address: "123 Universe Drive", capacity: 75, event_id: seed_event.id)
 
-#SapphireConf Rooms
-room_1 = sapphire_event.rooms.create(name: "Sun Room", room_number: "SUN", level: "12", address: "123 Universe Drive", capacity: 300, event_id: sapphire_event.id)
-room_2 = sapphire_event.rooms.create(name: "Moon Room", room_number: "MOON", level: "6", address: "123 Universe Drive", capacity: 150, event_id: sapphire_event.id)
-room_3 = sapphire_event.rooms.create(name: "Venus Theater", room_number: "VEN-T", level: "2", address: "123 Universe Drive", capacity: 75, event_id: sapphire_event.id)
-
-# SeedConf Event Team
+# Event Team
 seed_event.teammates.create(user: organizer, email: organizer.email, role: "organizer", state: Teammate::ACCEPTED, notifications: false)
 seed_event.teammates.create(user: track_director, email: track_director.email, role: "program team", state: Teammate::ACCEPTED)
 seed_event.teammates.create(user: reviewer, email: reviewer.email, role: "reviewer", state: Teammate::ACCEPTED)
 seed_event.teammates.create(user: speaker_reviewer, email: speaker_reviewer.email, role: "reviewer", state: Teammate::ACCEPTED)
 
-# SapphireConf Event Team
-sapphire_event.teammates.create(user: organizer, email: organizer.email, role: "organizer", state: Teammate::ACCEPTED)
-sapphire_event.teammates.create(user: track_director, email: track_director.email, role: "program team", state: Teammate::ACCEPTED)
-sapphire_event.teammates.create(user: reviewer, email: reviewer.email, role: "reviewer", state: Teammate::ACCEPTED)
-sapphire_event.teammates.create(user: speaker_reviewer, email: speaker_reviewer.email, role: "reviewer", state: Teammate::ACCEPTED)
-
-# Seed Conf Proposals
+# Proposals
 submitted_proposal_1 = seed_event.proposals.create(event_id: seed_event.id,
                                                    uuid: "abc123",
                                                    title: "Honey Bees",
@@ -150,7 +117,7 @@ withdrawn_proposal = seed_event.proposals.create(event_id: seed_event.id,
                                                  session_format_id: long_session_format.id,
                                                  track_id: track_1.id)
 
-# SeedConf Speakers
+# Speakers
 submitted_proposal_1.speakers.create(speaker_name: speaker_1.name, speaker_email: speaker_1.email, bio: "I am a speaker for cool events!", user_id: speaker_1.id, event_id: seed_event.id)
 submitted_proposal_1.speakers.create(speaker_name: speaker_2.name, speaker_email: speaker_2.email, bio: "I know a little bit about everything.", user_id: speaker_2.id, event_id: seed_event.id)
 submitted_proposal_2.speakers.create(speaker_name: speaker_2.name, speaker_email: speaker_2.email, bio: "I know a little bit about everything.", user_id: speaker_2.id, event_id: seed_event.id)
@@ -161,12 +128,41 @@ waitlisted_proposal.speakers.create(speaker_name: speaker_4.name, speaker_email:
 soft_rejected_proposal.speakers.create(speaker_name: speaker_5.name, speaker_email: speaker_5.email, bio: "I like cookies and rainbows.", user_id: speaker_5.id, event_id: seed_event.id)
 withdrawn_proposal.speakers.create(speaker_name: speaker_1.name, speaker_email: speaker_1.email, bio: "I am a speaker for cool events!", user_id: speaker_1.id, event_id: seed_event.id)
 
-
 # Taggings
- submitted_proposal_1.taggings.create(proposal_id: submitted_proposal_1.id, tag: "intermediate")
- accepted_proposal.taggings.create(proposal_id: accepted_proposal.id, tag: "beginner")
- waitlisted_proposal.taggings.create(proposal_id: waitlisted_proposal.id, tag: "beginner")
- soft_rejected_proposal.taggings.create(proposal_id: soft_rejected_proposal.id, tag: "beginner")
- withdrawn_proposal.taggings.create(proposal_id: withdrawn_proposal.id, tag: "advanced")
+submitted_proposal_1.taggings.create(proposal_id: submitted_proposal_1.id, tag: "intermediate")
+accepted_proposal.taggings.create(proposal_id: accepted_proposal.id, tag: "beginner")
+waitlisted_proposal.taggings.create(proposal_id: waitlisted_proposal.id, tag: "beginner")
+soft_rejected_proposal.taggings.create(proposal_id: soft_rejected_proposal.id, tag: "beginner")
+withdrawn_proposal.taggings.create(proposal_id: withdrawn_proposal.id, tag: "advanced")
 
- # Time Slots
+
+### SapphireConf -- this is an event in the early set-up stage
+sapphire_start_date = 10.months.from_now
+
+# Core Event Info
+sapphire_guidelines = %Q[
+# SapphireConf - The place to be in 2017!
+
+The first annual conference about the hot new programming language Sapphire
+is happening in moody Reykjavík, Iceland on February 1st, 2017!
+
+If you are on the cutting edge with savvy Sapphire skills, we want you!
+]
+
+sapphire_event = Event.create(name: "SapphireConf",
+                              slug: "sapphireconf",
+                              url: "https://en.wikipedia.org/wiki/Sapphire",
+                              contact_email: "info@sapphire.event",
+                              closes_at: 8.months.from_now,
+                              state: "draft",
+                              start_date: sapphire_start_date,
+                              end_date: sapphire_start_date + 1,
+                              guidelines: sapphire_guidelines,
+                              proposal_tags: %w(beginner intermediate advanced),
+                              review_tags: %w(beginner intermediate advanced))
+
+# Event Team
+sapphire_event.teammates.create(user: organizer, email: organizer.email, role: "organizer", state: Teammate::ACCEPTED)
+sapphire_event.teammates.create(user: track_director, email: track_director.email, role: "program team", state: Teammate::ACCEPTED)
+sapphire_event.teammates.create(user: reviewer, email: reviewer.email, role: "reviewer", state: Teammate::ACCEPTED)
+sapphire_event.teammates.create(user: speaker_reviewer, email: speaker_reviewer.email, role: "reviewer", state: Teammate::ACCEPTED)
